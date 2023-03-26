21b08e70d5c6b24fe8e66f2e0650be56dd23c0b2
Honey525 <jacobhanif71@gmail.com>
2023-03-26 04:45:04 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-5-printf.c
+#include <stdio.h>
+/**
+ * main - main entry point
+ *return: alwayse  0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,9 @@
 #include  <stdio.h>
-
 /**
  * main - this is the main funtion
  *
  * Return: 0 when successfull
  */
-
 int main (void)
 {
 		printf("with proper grammar, but the outcome is a piece of a,art, \n");
